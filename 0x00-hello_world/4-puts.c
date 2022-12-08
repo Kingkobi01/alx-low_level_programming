51542999e32f486ddb11e2fbb9eb9c7828d33f78
Kwabena Boakye <kwaboakye93@gmail.com>
2022-12-08 16:38:22 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <errno.h>
-/*
- * Printing out a string
- * */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
