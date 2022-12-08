838b13ce2fed255067d796f471f841e60591c369
Kwabena Boakye <kwaboakye93@gmail.com>
2022-12-08 17:47:28 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Entry point
+ * main - Print a sentence using puts
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-
 	return (0);
 }
