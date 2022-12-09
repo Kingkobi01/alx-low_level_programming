38b81efa45b0a96644bd594a73a5e4beffc5217e
Kwabena Boakye <kwaboakye93@gmail.com>
2022-12-09 22:52:15 +0000

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+/**
+ * Main - Print all possible
+ *	combinations of two
+ *	two-digit numbers
+ *	eg 00 01, 00 02, ..., 98 99
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int firstDigit;
+	int secondDigit;
+	int thirdDigit;
+	int lastDigit;
+
+	for (firstDigit = 0; firstDigit < 10; firstDigit++)
+	{
+		for (secondDigit = 0; secondDigit < 10; secondDigit++)
+		{
+			for (thirdDigit = 0; thirdDigit < 10; thirdDigit++)
+			{
+				for (lastDigit = 0; lastDigit < 10; lastDigit++)
+				{
+					if ((firstDigit < secondDigit)
+							&& (secondDigit < thirdDigit))
+					{
+						putchar(firstDigit + '0');
+						putchar(secondDigit + '0');
+						putchar(' ');
+						putchar(thirdDigit + '0');
+						putchar(lastDigit + '0');
+					}
+					else
+					{
+						continue;
+					}
+
+					if (!((firstDigit == 9) &&
+							(secondDigit == 8) &&
+							(thirdDigit == 9) &&
+							(lastDigit == 9)))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
