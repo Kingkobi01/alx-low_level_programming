a7523f4c4a5e4ab0c6a0730e0f9bca758f07368c
Kwabena Boakye <kwaboakye93@gmail.com>
2022-12-14 03:57:47 +0000

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - Prints the 9
+ * times table
+ *
+ * Return: void
+ */
+
+
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int firstNumber;
+	int lastNumber;
+	int product;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			product = i*j;
+
+			if (product > 9)
+			{
+				lastNumber = product % 10;
+				firstNumber = (product - lastNumber) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(firstNumber + '0');
+				_putchar(lastNumber + '0');
+
+			}
+			else
+			{
+				if (j != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+
+				_putchar(product + '0');
+			}
+		}
+
+		_putchar('\n');
+	}
+
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -53,6 +53,21 @@ This program contains a function that prints every minute of the day of Jack Bau
     * Prototype: `void jack_bauer(void);`
 
 
+9. [Learn your times table](./9-times_table.c)   
+THis program has a function which prints the 9 times tabe with 0,
+in this format    
+```
+0,  0,  0,  0,  0,  0,  0,  0,  0,  0$
+0,  1,  2,  3,  4,  5,  6,  7,  8,  9$
+0,  2,  4,  6,  8, 10, 12, 14, 16, 18$
+0,  3,  6,  9, 12, 15, 18, 21, 24, 27$
+0,  4,  8, 12, 16, 20, 24, 28, 32, 36$
+0,  5, 10, 15, 20, 25, 30, 35, 40, 45$
+0,  6, 12, 18, 24, 30, 36, 42, 48, 54$
+0,  7, 14, 21, 28, 35, 42, 49, 56, 63$
+0,  8, 16, 24, 32, 40, 48, 56, 64, 72$
+0,  9, 18, 27, 36, 45, 54, 63, 72, 81$
+```
 
 
 
