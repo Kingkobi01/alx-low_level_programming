e75224f40c3f729ccad7e6861b39728b3f2469ab
Kwabena Boakye <kwaboakye93@gmail.com>
2022-12-14 15:24:09 +0000

    Even Liber Abbac

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,32 +1,28 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the first 52 fibonacci numbers
+  * main - Prints the sum of Even Fibonacci numbers
+  * less than 4000000.
   *
   * Return: Nothing!
   */
 int main(void)
 {
 	int i = 0;
-	long j = 1, k = 2, sum = 0;
+	long j = 1, k = 2, sum = k;
 
-	while (i < 50)
+	while (k + j < 4000000)
 	{
-		if (i == 1)
-			 sum += k;
-		else
-		{
-			k += j;
-			j = k - j;
-			if ((k <= 4000000) && (k % 2 == 0))
-			{
+		k += j;
+
+		if (k % 2 == 0)
 			sum += k;
-			}
-		}
+
+		j = k - j;
 
 		++i;
 	}
 
-	printf("\n");
+	printf("%ld\n", sum);
 	return (0);
 }
