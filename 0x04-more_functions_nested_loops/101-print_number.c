8f1398128ef658c51a05bbf87bd576d928f3fbaa
Kwabena Boakye <kwaboakye93@gmail.com>
2022-12-15 18:19:42 +0000

    Fixed 10-print_triangle.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,24 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_number - Prints an integer
- *
- * @n : int param
- *
- * Return : void
- */
-
+  * print_number - Prints an integer.
+  * @n: The integer to prints.
+  *
+  * Return: Nothing!
+  */
 void print_number(int n)
 {
+	unsigned int k = n;
+
 	if (n < 0)
 	{
+		n *= -1;
+		k = n;
 		_putchar('-');
-
-		n = -1 * n;
 	}
 
-	if (n > 9)
-		print_number(n / 10);
+	k /= 10;
 
-	_putchar((n % 10) + '0');
+	if (k != 0)
+		print_number(k);
+
+	_putchar((unsigned int) n % 10 + '0');
+
 }
