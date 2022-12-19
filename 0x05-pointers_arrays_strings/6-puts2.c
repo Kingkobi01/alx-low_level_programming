1f5a6770e85b36f0f69b6f3fc0949d8f64a6dffe
Kwabena Boakye <kwaboakye93@gmail.com>
2022-12-19 18:14:28 +0000

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,12 @@
 
 void puts2(char *str)
 {
-    int i =0;
-    while(str[i] != '\0')
-    {
-        putchar(str[i]);
-        i+=2;
-    }
+	int i =0;
+	while(str[i] != '\0')
+	{
+		putchar(str[i]);
+		i+=2;
+	}
+	putchar('\n');
 }
 
