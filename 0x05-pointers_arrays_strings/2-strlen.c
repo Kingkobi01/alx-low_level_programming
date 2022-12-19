af0beadd784689d8fc4b97c2a7402dd06a24d21f
Kwabena Boakye <kwaboakye93@gmail.com>
2022-12-19 15:41:53 +0000

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - Returns the length of a string.
+ *
+ * @s: Pointer if char param
+ *
+ * Return: int, length of the string
+ */
+
+int _strlen(char *s)
+{
+	int length = strlen(s);
+
+	return (length);
+}
+
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -4,3 +4,6 @@
 1.[ 98 Battery st.](./0-reset_to_98.c)
 
 2. [Don't swap horses in crossing a stream](./1-swap.c)
+
+3. [This report, by its very length, defends itself against the risk of being read](./2-strlen.c)
+
