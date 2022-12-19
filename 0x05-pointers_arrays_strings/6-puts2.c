612a8321cd41671b18e47a224f37eb36c35a0e6a
Kwabena Boakye <kwaboakye93@gmail.com>
2022-12-19 18:10:53 +0000

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * put2 - Prints every other character of a string,
+ *			starting with the first character, followed by a new line
+ * @str: *char param to be manipulated.
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+    int i =0;
+    while(str[i] != '\0')
+    {
+        putchar(str[i]);
+        i+=2;
+    }
+	printf("/n");
+}
+
