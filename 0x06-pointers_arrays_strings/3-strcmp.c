a6c7978ac895a48e1cb76ab5b8f8d04464baff75
Kwabena Boakye <kwaboakye93@gmail.com>
2022-12-21 23:35:06 +0000

    strncmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include <string.h>
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 0 if 0s1 == @s2;
+ *			x > 0 if @s1 > @s2
+ *			x < 0 if @s1 < @s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
