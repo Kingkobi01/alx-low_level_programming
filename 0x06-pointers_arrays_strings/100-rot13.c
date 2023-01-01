9671f07ffd6d406dd03312c37dce01e7122d26fc
Kwabena Boakye <kwaboakye93@gmail.com>
2023-01-01 20:36:25 +0000

    rot-13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * rot13 - encodes a string using rot13.
+ * @c : char param
+ * Return : an encoded form of @c
+ */
+
+char *rot13(char *c)
+{
+    char *enC = c;
+    int i = 0;
+
+    while (c[i] != '\0')
+    {
+        if (isalpha(c[i]))
+        {
+
+
+            enC[i] = (c[i] - 'a' + 13) % 26 + 'a';
+        }
+        else
+        {
+
+            enC[i] = c[i];
+        }
+        i++;
+    }
+    return enC;
+}
