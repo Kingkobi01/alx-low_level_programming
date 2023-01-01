0cb280565844e606768c93d71ddeeb40b651682f
Kwabena Boakye <kwaboakye93@gmail.com>
2023-01-01 20:53:39 +0000

    rot-13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,28 +4,25 @@
 /**
  * rot13 - encodes a string using rot13.
  * @c : char param
- * Return : an encoded form of @c
+ * Return: an encoded form of @c
  */
 
-char *rot13(char *c)
+char *rot13(*c)
 {
-    char *enC = c;
-    int i = 0;
+	char *enC = c;
+	int i = 0;
 
-    while (c[i] != '\0')
-    {
-        if (isalpha(c[i]))
-        {
-
-
-            enC[i] = (c[i] - 'a' + 13) % 26 + 'a';
-        }
-        else
-        {
-
-            enC[i] = c[i];
-        }
-        i++;
-    }
-    return enC;
+	while (c[i] != '\0')
+	{
+	if (isalpha(c[i]))
+	{
+		enC[i] = (c[i] - a + 13) % 26 + 'a';
+	}
+	else
+	{
+	enC[i] c[i];
+	}
+	i++;
+	}
+	return (enC);
 }
