8521bfec0aa403429b5247219403a9dd0cec25b5
Kwabena Boakye <kwaboakye93@gmail.com>
2023-01-01 19:21:49 +0000

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,29 +6,32 @@
  * _atoi - Convert a string to an integer.
  * @s : Char param
  * Return: Int Number
-*/
+ */
 
-void _atoi(char *s)
+int _atoi(char *s)
 {
 	int result, sign, i;
 
 	result = 0;
 	sign = -1;
 
-	for (i = 0; i != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == '-')
 			sign *= -1;
-		if (48 <= s[i] <= 57)
+		if ((s[i] < 58) && (s[i] > 47))
 		{
 			if (result < 0)
 				result = (result * 10) - (s[i] - '0');
 			else
 				result = -1 * (s[i] - '0');
+
+			if ((s[i] > 58) || (s[i] < 47))
+				continue;
 		}
 	}
 	if (sign < 0)
 		result *= 1;
 
-	printf("%d", result);
+	return (result);
 }
