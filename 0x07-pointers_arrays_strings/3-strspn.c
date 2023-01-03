875d1e16fc61873b0504b172d72238d21908a031
Kwabena Boakye <kwaboakye93@gmail.com>
2023-01-03 16:27:44 +0000

    The line of life is a ragged diagonal between duty and desire

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * _strspn - gets the length of a prefix substring.
- * 			Works like a strspn
- * @s : a pointer to the string to be searched.
- * @accept : a pointer to a string containing
- * 			 the set of characters to be searched for
- * Return: number of bytes in the initial segment of @s
- * 			which consist only bytes from @accept
+ *_strspn - gets the length of a prefix substring.
+ *			Works like a strspn
+ *@s : a pointer to the string to be searched.
+ *@accept : a pointer to a string containing
+ *			 the set of characters to be searched for
+ *Return: number of bytes in the initial segment of @s
+ *			which consist only bytes from @accept
  */
 
 unsigned int _strspn(char *s, char *accept)
