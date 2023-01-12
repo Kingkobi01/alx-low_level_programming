3d858c264178c32af76d47aab124e4549320a521
Kwabena Boakye <kwaboakye93@gmail.com>
2023-01-12 13:41:27 +0000

    array_range

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,11 +13,19 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
 
+	if (nmemb == 0 || size == 0)
+	{
+		ptr = malloc(1);
+
+		if (!ptr)
+			return (NULL);
+
+		memset(ptr, 0, 1);
+		return (ptr);
+	}
+
 	if (nmemb > (UINT_MAX / size))
 		return (NULL);
-
-	if (nmemb == 0 || size == 0)
-		ptr = malloc(1);
 
 	ptr = malloc(nmemb * size);
 
