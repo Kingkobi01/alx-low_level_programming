20eac76908c74205dfc60635e26d776b192b9666
Kwabena Boakye <kwaboakye93@gmail.com>
2023-01-16 14:03:57 +0000

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+/**
+ * struct dog - ...
+ * @name : ...
+ * @age : ...
+ * @owner : ...
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
