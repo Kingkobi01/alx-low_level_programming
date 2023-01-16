e6c86f9acbf38aa5e1c3ed0cce15d5771c245027
Kwabena Boakye <kwaboakye93@gmail.com>
2023-01-16 12:24:21 +0000

    SUM

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+/*
+* main - ...
+* Return: nothing
+*/
+
 
 int main(void)
 {
 	printf("%s\n",__FILE__);
 	return(0);
 }
+
 
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,4 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+#define SUM(X,Y) ((X) + (Y));
+#endif;
