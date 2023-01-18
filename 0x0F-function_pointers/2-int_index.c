1382d519c47ee3cfec82b2f62825f24ffae1cfb1
Kwabena Boakye <kwaboakye93@gmail.com>
2023-01-18 15:06:45 +0000

    To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+
+/**
+ * int_index - ...
+ * @array : ...
+ * @size : ...
+ * @cmp : ...
+ * Return: ...
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	i = 0;
+
+	if (size <= 0)
+		return (-1);
+
+	for (; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+
+	return (-1);
+}
