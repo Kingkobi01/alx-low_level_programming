f13baa6ed5083b5658e456c68388a6ca66974294
Kwabena Boakye <kwaboakye93@gmail.com>
2023-01-26 18:55:18 +0000

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * my_startup_function - prints a sentence
+ *		before the main function is called
+ * Return: nothing
+ */
+
+void my_startup_function(void)
+{
+	printf(
+		"You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
