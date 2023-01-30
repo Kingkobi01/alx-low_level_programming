04e09ce7db7f2ba673f89a8d76a9598f6131f41c
Kwabena Boakye <kwaboakye93@gmail.com>
2023-01-30 17:58:18 +0000

    List length

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints all the elements
+ * of a listint_t list.
+ * @h : Address of the head of the listint_t list.
+ * Return: Number of elements in the list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+
+	count = 0;
+
+	if (h != NULL)
+	{
+
+		while (h)
+		{
+			count += 1;
+			printf("%d\n", h->n);
+			h = h->next;
+		}
+
+		return (count);
+	}
+	return (-1);
+}
