6f3d0f6cb988f092f51ffd355df32ff3a3f7a5b3
Kwabena Boakye <kwaboakye93@gmail.com>
2023-01-30 17:58:47 +0000

    Add node

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * listint_len - Returns the number of elements
+ * in a linked listint_t list.
+ * @h : Address of the head of the listint_t list.
+ * Return: Number of elements in the list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count;
+
+	count = 0;
+
+	if (h != NULL)
+	{
+
+		while (h)
+		{
+			count += 1;
+
+			h = h->next;
+		}
+
+		return (count);
+	}
+	return (-1);
+}
