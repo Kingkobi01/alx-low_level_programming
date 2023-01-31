b14afe1a0f74e1f0aa6ddf81cd10052a659b2795
Kwabena Boakye <kwaboakye93@gmail.com>
2023-01-31 16:52:01 +0000

    Add node

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,12 +12,18 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (*head)
+	if (head)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		while (*head)
+		{
+			temp = (*head)->next;
+			free(*head);
+			*head = temp;
+		}
 	}
+	else
+		return;
+	free(*head);
 
 	head = NULL;
 }
