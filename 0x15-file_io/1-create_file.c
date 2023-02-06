7c90838f0774d71c179be21b0745415da2a40ac2
Kwabena Boakye <kwaboakye93@gmail.com>
2023-02-06 20:41:45 +0000

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * create_file - ...
+ * @filename :....
+ * @text_content : ...
+ * Return: ...
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+		write(fd, text_content, _strlen(text_content));
+
+	close(fd);
+	return (1);
+}
+
+/**
+ * _strlen - ...
+ * @str
+ */
+
+int _strlen(char *str)
+{
+	int i = 0, count = 0;
+
+	while (str[i])
+	{
+		count++;
+		i++;
+	}
+
+	return (count);
+}
