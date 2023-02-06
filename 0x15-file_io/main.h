66801598f40c986dcd362adddc9236279f6bcfcf
Kwabena Boakye <kwaboakye93@gmail.com>
2023-02-06 23:28:36 +0000

    cp

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,11 +4,14 @@
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#define BUFFER_SIZE 1024
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
+void copy(const char *src, const char *dest);
 #endif
