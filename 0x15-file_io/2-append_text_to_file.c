dd4684a86a6b7020a972d6ba8563026fdeaa592e
Kwabena Boakye <kwaboakye93@gmail.com>
2023-02-06 21:32:55 +0000

    Speak gently, she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - ...
+ * @filename : ...
+ * @text_content : ...
+ * Return: ...
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	fd = open(filename, O_APPEND | O_WRONLY);
+
+	if (fd != -1)
+	{
+		if (write(fd, text_content, _strlen(text_content)) == -1)
+			return (-1);
+	}
+	else
+		return (-1);
+
+	close(fd);
+	return (1);
+}
+
+/**
+ * _strlen - ...
+ * @str :...
+ * Return: ...
+ */
+
+int _strlen(char *str)
+{
+	int i = 0, count = 0;
+
+	while (str[i])
+	{
+		count++;
+		i++;
+	}
+
+	return (count);
+}
